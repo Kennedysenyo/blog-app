8894e341f9db26abc86a2265d08f0d8acfcb6b26
Kennedysenyo <kennedysenyo@gmail.com>
2025-03-13 23:46:10 +0000

    added get and post route handlers

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -2,11 +2,24 @@ import { db } from "@/db"
 import { blogs } from "@/db/schema";
 import { NextResponse } from "next/server";
 
+
+// GET all blogs
 export async function GET() {
-  const allBlogs = await db.select().from(blogs);
-  return NextResponse.json(allBlogs)
+  try {
+    const allBlogs = await db.select().from(blogs);
+    return NextResponse.json(allBlogs)
+  }catch(error) {
+    return NextResponse.json({error: "Failed to fetch blogs"}, {status: 500})
+  }
 }
 
+// Post a new blog
 export async function POST(req: Request) {
-
+  try{
+    const {title, content} = await req.json();
+    const newBlog = await db.insert(blogs).values({ title, content}).returning();
+    return NextResponse.json(newBlog);
+  }catch {
+    return NextResponse.json({error: "Failed to create post"}, {status: 500});
+  }
 }
